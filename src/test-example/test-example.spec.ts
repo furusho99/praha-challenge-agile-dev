345e24684b23928bb77911942e132d60d4a1578a
Kou <hikkappi@yahoo.co.jp>
2025-05-18 14:44:20 +0900

    Refactor：prettierを実行。

--- src/test-example/test-example.spec.ts
+++ src/test-example/test-example.spec.ts
@@ -1,7 +1,7 @@
-import { describe, it, expect } from 'vitest';
+import { describe, it, expect } from "vitest";
 
-describe('Example', () => {
-  it('should be true', () => {
+describe("Example", () => {
+  it("should be true", () => {
     expect(true).toBe(true);
   });
 });
