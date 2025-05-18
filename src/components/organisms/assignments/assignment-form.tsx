23d660d37eab18b78adb3d451f99b8f2301d847c
Kou <hikkappi@yahoo.co.jp>
2025-05-18 12:54:48 +0900

    Remove: 不要なuseRouterインポートを削除

--- src/components/organisms/assignments/assignment-form.tsx
+++ src/components/organisms/assignments/assignment-form.tsx
@@ -1,7 +1,6 @@
 "use client"
 
 import { useState } from "react"
-import { useRouter } from "next/navigation"
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
 import * as z from "zod"
@@ -44,7 +43,6 @@ const formSchema = z.object({
 })
 
 export default function AssignmentForm() {
-  const router = useRouter()
   const [isSubmitting, setIsSubmitting] = useState(false)
 
   const form = useForm<z.infer<typeof formSchema>>({
