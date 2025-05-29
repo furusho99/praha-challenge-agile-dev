d143418e867d0d19d343dc994a07d48fbf70a178
furusho99 <xguye2514@gmail.com>
2025-05-29 19:11:34 +0900

    impl signup & fix sign in

---
+++ src/validations/userSchema.ts
@@ -0,0 +1,19 @@
+import { z } from "zod";
+import { emailSchema } from "./emailSchema";
+import { userNameSchema } from "./userNameSchema";
+
+export const userSchema = z.object({
+  id: z.string().uuid({ message: "有効なUUID形式ではありません" }),
+  email: emailSchema,
+  firstName: userNameSchema("名"),
+  lastName: userNameSchema("姓"),
+  season: z
+    .number()
+    .int()
+    .min(1, { message: "シーズンは1以上の整数でなければなりません" })
+    .max(100, { message: "シーズンは100以下の整数でなければなりません" }),
+  status: z.enum(["ACTIVE", "INACTIVE"], {
+    message: "ステータスはACTIVEまたはINACTIVEでなければなりません",
+  }),
+});
+export type SignUpSchema = z.infer<typeof userSchema>;
