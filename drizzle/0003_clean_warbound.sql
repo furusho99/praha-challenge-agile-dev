cf74dfaa512131bf0eb019089ec0916db3567884
Kou <hikkappi@yahoo.co.jp>
2025-06-11 23:46:14 +0900

    Add: seasonsテーブルとteamsテーブルを追加し、usersテーブルにteam_idカラムを追加

---
+++ drizzle/0003_clean_warbound.sql
@@ -0,0 +1,19 @@
+CREATE TABLE "seasons" (
+	"id" uuid PRIMARY KEY NOT NULL,
+	"name" varchar(100) NOT NULL,
+	"created_at" timestamp DEFAULT now() NOT NULL,
+	"updated_at" timestamp DEFAULT now() NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE "teams" (
+	"id" uuid PRIMARY KEY NOT NULL,
+	"name" varchar(255) NOT NULL,
+	"season_id" uuid NOT NULL,
+	"created_at" timestamp DEFAULT now() NOT NULL,
+	"updated_at" timestamp DEFAULT now() NOT NULL
+);
+--> statement-breakpoint
+ALTER TABLE "users" ADD COLUMN "team_id" uuid NOT NULL;--> statement-breakpoint
+ALTER TABLE "teams" ADD CONSTRAINT "teams_season_id_seasons_id_fk" FOREIGN KEY ("season_id") REFERENCES "public"."seasons"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "users" ADD CONSTRAINT "users_team_id_teams_id_fk" FOREIGN KEY ("team_id") REFERENCES "public"."teams"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "users" DROP COLUMN "season";
