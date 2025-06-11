a4856b351b1de7b3a52b5df94889ea2ae1044806
Kou <hikkappi@yahoo.co.jp>
2025-06-12 00:27:59 +0900

    Add: seasonsテーブルとteamsテーブルを作成し、usersテーブルにteam_idカラムを追加

--- drizzle/0003_past_hammerhead.sql
+++ drizzle/0003_past_hammerhead.sql
@@ -13,7 +13,7 @@ CREATE TABLE "teams" (
 	"updated_at" timestamp DEFAULT now() NOT NULL
 );
 --> statement-breakpoint
-ALTER TABLE "users" ADD COLUMN "team_id" uuid NOT NULL;--> statement-breakpoint
+ALTER TABLE "users" ADD COLUMN "team_id" uuid;--> statement-breakpoint
 ALTER TABLE "teams" ADD CONSTRAINT "teams_season_id_seasons_id_fk" FOREIGN KEY ("season_id") REFERENCES "public"."seasons"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
 ALTER TABLE "users" ADD CONSTRAINT "users_team_id_teams_id_fk" FOREIGN KEY ("team_id") REFERENCES "public"."teams"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
 ALTER TABLE "users" DROP COLUMN "season";
